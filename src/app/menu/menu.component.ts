6af9ea782cca135eb18706774c106dd61e6a5389
Carlos <caedvica86@hotmail.com>
2018-08-22 22:51:06 -0500

    se agregan componentes con modelos ejemplos

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  arrItem : String[];
+  constructor() { 
+    this.arrItem = ['inicio','acerca','ayuda','cliente']
+  }
+
+  ngOnInit() {
+  }
+
+}
