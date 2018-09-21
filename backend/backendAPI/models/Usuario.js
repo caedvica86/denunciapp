ba4204ffff5f50e592939ffc3e5b4caa5069a55a
caedvica86 <caedvica86@hotmail.com>
2018-09-20 21:36:24 -0500

    get y post test

--- backend/backendAPI/models/Usuario.js
+++ backend/backendAPI/models/Usuario.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql');
 conn = mysql.createConnection({
-    host: 'localhost',
+    host: '192.168.0.100',
     port: '3306',
     user: 'root',
     password: 'cencabo2016',
     database: 'fedesoft'
 })
 let userModel = {};
+
 
 userModel.getUsuario = (callback) => {
     if(conn){
@@ -17,6 +18,23 @@ userModel.getUsuario = (callback) => {
             }
             else{
                 callback(null,rows);
+            }
+             
+        }
+    )}
+};
+
+userModel.insertUser = (userData,(callback) => {  
+    if(conn){
+        conn.query('INSERT INTO usuario ?', userData,
+        (error,result)=> {
+            if(error){
+                throw error;
+            }
+            else{
+                callback(null,{
+                    'insertId' : result.insertId
+                })
             }
              
         }
--- backend/backendAPI/routes/users.js
+++ backend/backendAPI/routes/users.js
@@ -5,8 +5,35 @@ var usu = require('../models/Usuario')
 router.get('/', function(req, res, next) {
   //res.json({users: [{name: 'Timmy'}]});
   usu.getUsuario((error,data)=>{
-    res.status(200).json;
+    res.status(200).json(data);
   })
+});
+
+router.post('/', function(req, res, next) {
+  //console.log(req.body);
+  const userData = {
+    Id : null,
+    Nombre: req.body.Nombre,
+    Clave : req.body.Clave
+  };  
+
+  usu.insertUser(userData,(error,data)=>{
+    if(data&& data.insertId){
+      res.json({
+        sucess : true,
+        msg : "Usuario Creado",
+        data : data
+    })
+  }
+  else{
+    res.json({
+        sucess: false,
+        msg : "Error en la creacion del usuario",
+        data: error
+    })
+  }
+
+})
 });
 
 module.exports = router;
